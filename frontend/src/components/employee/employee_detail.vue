b512eba41c7b7c493cc57aa4a4e618d8a6f240dd
james30108 <independente30108@gmail.com>
2022-07-04 17:17:26 +0700

    4-7-65

---
+++ frontend/src/components/employee/employee_detail.vue
@@ -0,0 +1,19 @@
+<script>
+import services_employee from "../../services/setting_employee";
+
+export default {
+    name: "Employee's Detail",
+    data () {
+        return {
+            id : 
+        }
+    }
+}
+
+
+</script>
+
+<template>
+    {{ JSON.stringify(id) }}
+    Employee Detail
+</template>
